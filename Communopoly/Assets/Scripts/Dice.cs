aa46f728efc121368fc8ad6f3d317b83377ecb65
rizthesnuggie <77313160+rizthesnuggie@users.noreply.github.com>
2021-01-12 00:31:47 -0500

    Dice scipt

---
+++ Communopoly/Assets/Scripts/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+
+    public int dice; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            int dice = Random.Range(2, 13);
+            Debug.Log(dice);
+        }
+    }
+}
