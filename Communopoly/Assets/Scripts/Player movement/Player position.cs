29d3f55f07f3a333b7cc998ec09668b0157eb7c2
thelightdesigner <48635344+thelightdesigner@users.noreply.github.com>
2021-01-12 00:36:10 -0500

    hi

--- Communopoly/Assets/Scripts/Player movement/Player position.cs
+++ Communopoly/Assets/Scripts/Player movement/Player position.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Playerposition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public Playerposition(string Name, int Spaces)
     {
-        
+        this.Name = Name;
+        BoardPos = 0;
+        Position = null;
+        _Board = new Board() { Spaces = Spaces};
     }
+    public string Name { get; set; }
+    public int BoardPos { get; set; }
+    public Vector3? Position { get; set; }
+    public Board _Board { get; set; }
+
+    public void AddPos(int Pos)
+    {
+        BoardPos += Coterminal(Pos);
+    }
+    public int Coterminal(int Pos)
+    {
+        if (Pos > _Board.Spaces) return Pos - _Board.Spaces;
+        return Pos;
+    }
+}
+public class Board
+{
+    public int Spaces { get; set; }
+    public Vector2 Dimensions { get; set; }
 }
