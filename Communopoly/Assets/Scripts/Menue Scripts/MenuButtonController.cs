3d02ae5f8709c3e43290798eb51a0d8e08c4702d
rizthesnuggie <77313160+rizthesnuggie@users.noreply.github.com>
2021-01-15 23:21:15 -0500

    Added prototype menue and rought menu controls

---
+++ Communopoly/Assets/Scripts/Menue Scripts/MenuButtonController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public GameObject escon;
+    public Button yes;
+    public Button no;
+    public GameObject Mainbut;
+
+    public void Play()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Exit1()
+    {
+        Mainbut.SetActive(false);
+        //Exit();
+    }
+
+   /* public void Exit()
+    {
+        Time.timeScale = 0;
+        escon.SetActive(true);
+        if (exit == true)
+        {
+            Application.Quit();
+        }
+        else if (exit == false)
+        {
+            //escon.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+    }
+   */
+
+    public enum exit
+    {
+
+    }
+}
+
+
+
